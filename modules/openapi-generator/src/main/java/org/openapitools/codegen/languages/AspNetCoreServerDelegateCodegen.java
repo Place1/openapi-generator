e18a2e6c0c5ea48d23e3a0101be4ea6ba630ffd3
James Batt <getjamesbatt@gmail.com>
2019-02-27 12:18:12 +1100

    added aspnetcore-delegate generator

---
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/AspNetCoreServerDelegateCodegen.java
@@ -0,0 +1,151 @@
+/*
+ * Copyright 2018 OpenAPI-Generator Contributors (https://openapi-generator.tech)
+ * Copyright 2018 SmartBear Software
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openapitools.codegen.languages;
+
+import com.samskivert.mustache.Mustache;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.openapitools.codegen.CodegenConstants;
+import org.openapitools.codegen.CodegenOperation;
+import org.openapitools.codegen.CodegenType;
+import org.openapitools.codegen.SupportingFile;
+import org.openapitools.codegen.utils.URLPathUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Map;
+
+import static java.util.UUID.randomUUID;
+
+public class AspNetCoreServerDelegateCodegen extends AbstractCSharpCodegen {
+
+    public static final String USE_SWASHBUCKLE = "useSwashbuckle";
+    public static final String ASPNET_CORE_VERSION = "aspnetCoreVersion";
+
+    private String packageGuid = "{" + randomUUID().toString().toUpperCase(Locale.ROOT) + "}";
+
+    @SuppressWarnings("hiding")
+    protected Logger LOGGER = LoggerFactory.getLogger(AspNetCoreServerDelegateCodegen.class);
+
+    public AspNetCoreServerDelegateCodegen() {
+        super();
+
+        outputFolder = "generated-code" + File.separator + getName();
+
+        modelTemplateFiles.put("model.mustache", ".cs");
+        apiTemplateFiles.put("controller.mustache", ".cs");
+
+        embeddedTemplateDir = templateDir = "aspnetcoredelegate";
+
+        // contextually reserved words
+        // NOTE: C# uses camel cased reserved words, while models are title cased. We don't want lowercase comparisons.
+        reservedWords.addAll(
+                Arrays.asList("var", "async", "await", "dynamic", "yield")
+        );
+
+        cliOptions.clear();
+
+        // CLI options
+        addOption(CodegenConstants.PACKAGE_NAME,
+                "C# package name (convention: Title.Case).",
+                packageName);
+
+        // CLI Switches
+        addSwitch(CodegenConstants.SORT_PARAMS_BY_REQUIRED_FLAG,
+                CodegenConstants.SORT_PARAMS_BY_REQUIRED_FLAG_DESC,
+                sortParamsByRequiredFlag);
+
+        addSwitch(CodegenConstants.USE_DATETIME_OFFSET,
+                CodegenConstants.USE_DATETIME_OFFSET_DESC,
+                useDateTimeOffsetFlag);
+
+        addSwitch(CodegenConstants.USE_COLLECTION,
+                CodegenConstants.USE_COLLECTION_DESC,
+                useCollection);
+
+        addSwitch(CodegenConstants.RETURN_ICOLLECTION,
+                CodegenConstants.RETURN_ICOLLECTION_DESC,
+                returnICollection);
+
+    }
+
+    @Override
+    public CodegenType getTag() {
+        return CodegenType.SERVER;
+    }
+
+    @Override
+    public String getName() {
+        return "aspnetcoredelegate";
+    }
+
+    @Override
+    public String getHelp() {
+        return "Generates an ASP.NET Core Web API server.";
+    }
+
+    @Override
+    public void processOpts() {
+        super.processOpts();
+        apiPackage = packageName + ".Controllers";
+        modelPackage = packageName + ".Models";
+
+        supportingFiles.add(new SupportingFile("validateModel.mustache", "Runtime", "ValidateModelStateAttribute.cs"));
+    }
+
+    @Override
+    public String apiFileFolder() {
+        return outputFolder + File.separator + "Controllers";
+    }
+
+    @Override
+    public String modelFileFolder() {
+        return outputFolder + File.separator + "Models";
+    }
+
+    @Override
+    protected void processOperation(CodegenOperation operation) {
+        super.processOperation(operation);
+
+        // HACK: Unlikely in the wild, but we need to clean operation paths for MVC Routing
+        if (operation.path != null) {
+            String original = operation.path;
+            operation.path = operation.path.replace("?", "/");
+            if (!original.equals(operation.path)) {
+                LOGGER.warn("Normalized " + original + " to " + operation.path + ". Please verify generated source.");
+            }
+        }
+
+        // Converts, for example, PUT to HttpPut for controller attributes
+        operation.httpMethod = "Http" + operation.httpMethod.substring(0, 1) + operation.httpMethod.substring(1).toLowerCase(Locale.ROOT);
+    }
+
+    @Override
+    public Mustache.Compiler processCompiler(Mustache.Compiler compiler) {
+        // To avoid unexpected behaviors when options are passed programmatically such as { "useCollection": "" }
+        return super.processCompiler(compiler).emptyStringIsFalse(true);
+    }
+
+    @Override
+    public String toRegularExpression(String pattern) {
+        return escapeText(pattern);
+    }
+}
